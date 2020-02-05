5df0e23b42cf1a63dab9f91617e65154f812d7ed
Stanislav Ch <flroup@gmail.com>
2020-02-05 20:06:53 +0600

    Couple of scripts

---
+++ class_logger.py
@@ -0,0 +1,67 @@
+from functools import wraps
+
+from datetime import datetime
+
+#Name of log file
+filename = 'logged-classes.log'
+
+
+#Base class for logging all calls of methods
+class Logger:
+
+    def _decorator(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            time_now = datetime.now().time().strftime('%H:%M:%S')
+            class_name = type(self).__name__
+            func_name = func.__name__
+            string = f"{time_now}: {class_name}, {func_name}, {args, kwargs}\n"
+
+            with open(filename, "a+") as file:
+                file.write(string)
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    def __getattribute__(self, item):
+        value = object.__getattribute__(self, item)
+        if callable(value):
+            decorator = object.__getattribute__(self, '_decorator')
+            return decorator(value)
+        return value
+
+
+class Child(Logger):
+
+    def test(self, a):
+        print(f"Test 1 = {a}")
+
+
+class Child2(Logger):
+
+    def test2(self, b):
+        print(f"Test 2 = {b}")
+
+
+class GrandChild(Child):
+
+    def test3(self, c):
+        print(f"Test 3 = {c}")
+
+
+if __name__ == "__main__":
+
+
+    # Tests
+
+    #Creattion instances of Classes
+    a = Child()
+    b = Child2()
+    c = GrandChild()
+
+    #Calls of methods of Classes
+    a.test(5)
+    b.test2(6)
+    c.test3(7)
+    c.test(8)
---
+++ my_tail.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3.8
+
+'''
+This scrip is similar with tail.
+Script reads first 5 strings (counting the string
+is not bigger 255 chars) from the file and after show actual
+information from the file until interrupt
+'''
+
+from sys import argv
+
+from os import  stat
+
+#Calculate start position for reading file
+def calculate_start_position(filesize, number_of_strings):
+    number_of_symbols = number_of_strings*255
+    buffer = filesize -  number_of_symbols
+    return buffer if filesize > number_of_symbols else 0
+
+#Read file from position and until the end
+def read_file(filename, position):
+    with open(filename) as f:
+        f.seek(position)
+        string_array = []
+        for s in f:
+            string_array.append(s)
+        return string_array, f.tell()
+
+
+if __name__ == '__main__':
+
+    try:
+        filename = argv[1]
+        filesize = stat(filename).st_size
+
+    except IndexError:
+        print("You should specify name of the file")
+
+    except FileNotFoundError:
+        print("There is no such file")
+
+    else:
+        number_of_strings = -5
+        start = calculate_start_position(filesize, number_of_strings)
+
+        #Endless loop for checking and output the file modifications
+        while True:
+
+            if stat(filename).st_size > start:
+                string_array, start = read_file(filename, start)
+                [print(s, end='') for s in string_array[number_of_strings:]]
+                number_of_strings = 0
+
+            elif stat(filename).st_size < start:
+                print("File size is reduced. Break.")
+                break
+
